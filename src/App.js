a4ce637b1b2e3fc769941e48267cf9655f6cb27d
Priyank <priyankcrjr7@gmail.com>
2022-04-06 11:54:34 +0530

    added bootstrap global css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Navbar } from "./shared/components";
 import { Landing, Features } from "./pages/Home/containers";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
